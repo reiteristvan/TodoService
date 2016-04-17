64d51201ad597e43cf49bb3b72647793f65eb8ff
reiteristvan <reiteristvan@gmail.com>
2016-04-17 18:49:39 +0200

    Project added

---
+++ TodoService/TodoService/Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TodoService.Controllers
+{
+    [RoutePrefix("api/todo")]
+    public class TodoController : ApiController
+    {
+        [HttpGet]
+        [Route("test")]
+        public string Test()
+        {
+            return "I am alive";
+        }
+    }
+}
---
+++ TodoService/TodoService/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TodoService.Startup))]
+
+namespace TodoService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
